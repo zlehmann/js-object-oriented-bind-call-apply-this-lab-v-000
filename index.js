7a99ad4d3aa53f236ae348680c29ff3b2d6fee8c
Jeff Katz <jeff@flatironschool.com>
2017-08-29 12:40:23 -0400

    update code

--- index.js
+++ index.js
@@ -1,64 +1,15 @@
-// driver has many trips
-// passenger has many trips
-// trips
-  // passenger
-  // driver
-  // belongs to scooter
-
-let store = {drivers: [], passengers: [], trips: []}
-
-let driverId = 0
-class Driver {
-  constructor(name, zipcode){
-    this.name = name
-    this.id = ++driverId
-    store.drivers.push(this)
-  }
-  trips(){
-    return store.trips.filter((trip)=> {
-      return trip.driverId == this.id
-    })
-  }
-  passengers(){
-    return this.trips().map((trip)=> {
-      return trip.passenger()
-    })
-  }
+function justInvoke(fn){
+  return fn()
 }
 
-let passengerId = 0
-
-class Passenger {
-  constructor(name){
-    this.name = name
-    this.id = ++passengerId
-    store.passengers.push(this)
-  }
-
-  trips(){
-    return store.trips.filter((trip)=> {
-      return trip.passengerId == this.id
-    })
-  }
-  drivers(){
-    return this.trips().map((trip)=> {
-      return trip.driver()
-    })
-  }
+function setThisWithCall(fn, thisValue){
+  return fn.call(thisValue)
 }
 
-let tripId = 0
-class Trip {
-  constructor(driver, passenger, startingZip, endingZip){
-    this.driverId = driverId;
-    this.passengerId = passengerId;
-    this.id = ++tripId
-    store.trips.push(this)
-  }
-  driver(){
-    return store.drivers.find((driver) => { return driver.id === this.driverId })
-  }
-  passenger(){
-    return store.passengers.find((passenger) => { return passenger.id === this.passengerId })
-  }
+function setThisWithApply(fn, thisValue, args){
+  return fn.apply(thisValue, args)
+}
+
+function returnNewFunctionOf(functionToBeCopied, thisValue){
+  return functionToBeCopied.bind(thisValue)
 }
