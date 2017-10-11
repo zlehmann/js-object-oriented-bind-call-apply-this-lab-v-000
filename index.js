5fc37ab2ad0fa9d321f201c50828c3c052fca04d
Daniel Seehausen <Daniel.Seehausen@gmail.com>
2017-10-11 14:37:00 -0400

    solution branch was for a different lab. Replaced entirely with master lab and provided solution

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-//Your code here
+function justInvoke(fn) {
+  return fn();
+}
+
+function setThisWithCall(fn, thisValue, argument) {
+  return fn.call(thisValue, argument);
+}
+
+function setThisWithApply(fn, thisValue, ourArguments) {
+  return fn.apply(thisValue, ourArguments);
+}
+
+function returnNewFunctionOf(functionToBeCopied, thisValue) {
+  const copy = functionToBeCopied.bind(thisValue);
+  return copy;
+}
