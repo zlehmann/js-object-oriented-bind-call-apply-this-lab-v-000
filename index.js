8a2f68ef3fc89bfcc7cc28e082198966e12fdf0b
Zachary Lehmann <zlehmann@gmail.com>
2019-03-23 23:08:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 //Your code here
+function justInvoke(fn) {
+  return fn.call();
+}
+
+function setThisWithCall(fn, thisValue, arg) {
+  return fn.call(thisValue, 18);
+}
+
+function setThisWithApply(fn, thisValue, args) {
+  return fn.apply(thisValue, args)
+}
+
+function returnNewFunctionOf(functionToBeCoped, thisValue) {
+  const result = functionToBeCoped.bind(thisValue);
+  return result;
+}
